4a44b4bc47eaef96a63524d8d9d3083a7e97a51e
PC <gudsanta66@gmail.com>
2023-04-23 19:16:11 +0500

    Проектная работа 1. Вторая версия

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -6,24 +6,34 @@ import java.util.Scanner;
 import javax.swing.Spring;
 
 public class Calc {
-    private Scanner scanner;
-    private List<Product> products = new ArrayList<>();
+    private final Scanner scanner;
+    private final List<Product> products = new ArrayList<>();
 
     private double commonPrice;
 
-    Calc (Scanner scanner){
+    Calc(Scanner scanner) {
         this.scanner = scanner;
     }
 
-    public void addProductToCalc(){
+    public double enterProductPrice() {
+        try {
+            Scanner scanner1 = new Scanner(System.in);
+            return scanner1.nextDouble();
+        } catch (Exception e) {
+            print("некорректное значение! пожалуйста, введите число");
+            return enterProductPrice();
+        }
+    }
 
-        while (true){
+    public void addProductToCalc() {
+
+        while (true) {
 
             print("Пожалуйста добавьте товар");
             print("введите название товара");
             String productName = scanner.next();
             print("введите стоимость товара");
-            double productPrice = scanner.nextDouble();
+            double productPrice = enterProductPrice();
             if (productPrice >= 0) {
                 Product product = new Product(productName, productPrice);
                 products.add(product);
@@ -31,47 +41,51 @@ public class Calc {
                 commonPrice = commonPrice + productPrice;
             }
             print("текущая общая сумма " + commonPrice);
-            print("добавить еще один товар?");
+            print("добавить еще один товар?" + '\n' + "для того чтобы закончить добавлять товары введите \"завершить\"");
             String addAnotherProduct = scanner.next();
             String complete = "Завершить";
-            if (addAnotherProduct.equalsIgnoreCase(complete)){
+            if (addAnotherProduct.equalsIgnoreCase(complete)) {
                 break;
             }
 
         }
     }
-    public void printProducts(){
+
+    public void printProducts() {
         print("Добавленные товары:");
-        for (int i = 0; i < products.size(); i++){
+        for (int i = 0; i < products.size(); i++) {
             Product product = products.get(i);
             String name = product.name;
             double price = product.price;
-            print(name + " стоимостью " + price + '\n' + "Общая сумма: " + commonPrice + " " + caseRuble(commonPrice));
-
+            print(name + " стоимостью " + price + " " + caseRuble(price));
         }
+        print("\n" + "Общая сумма: " + commonPrice + " " + caseRuble(commonPrice));
     }
 
-    public void printAmountPerPerson(int quantityPerson){
-        double amount = commonPrice/quantityPerson;
+    public void printAmountPerPerson(int quantityPerson) {
+        double amount = commonPrice / quantityPerson;
         print("Cумма на человека " + amount + " " + caseRuble(amount));
     }
-    private void print (String message){
+
+    private void print(String message) {
         System.out.println(message);
     }
 
-    private String caseRuble (double commonPrice) {
+    private String caseRuble(double commonPrice) {
 
         String ruble;
 
-        if (commonPrice >= 0 && commonPrice < 2) {
-           ruble = "рубль";
-       } else if (commonPrice >= 2 && commonPrice < 5) {
-           ruble = "рубля";
-       } else if (commonPrice >= 5){
-           ruble = "рублей";
-       } else {
+        if (commonPrice >= 1 && commonPrice < 2) {
+            ruble = "рубль";
+        } else if (commonPrice >= 2 && commonPrice < 5) {
+            ruble = "рубля";
+        } else if (commonPrice >= 5) {
+            ruble = "рублей";
+        } else if (commonPrice < 1 && commonPrice > 0) {
+            ruble = "рубля";
+        } else {
             ruble = "неверная сумма";
         }
-       return ruble;
+        return ruble;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,29 +4,44 @@ public class Main {
     public static void main(String[] args) {
         print("На сколько человек необходимо разделить счёт?");
         Scanner scanner = new Scanner(System.in);
-        Calc calc = new Calc(scanner);
-        int quantityUser = scanner.nextInt();
-        enterQuantityUser(quantityUser);
+        Scanner scannerForCalc = new Scanner(System.in);
+        int quantityUser = quantityUserChecker(enterQuantityUser(scanner), scanner);
+        Calc calc = new Calc(scannerForCalc);
         calc.addProductToCalc();
         calc.printProducts();
         calc.printAmountPerPerson(quantityUser);
-
-
     }
-    private static void enterQuantityUser(int quantityUser){
-        while (true){
+
+    private static int enterQuantityUser(Scanner scanner) {
+        try {
+            return scanner.nextInt();
+
+        } catch (Exception e) {
+            print("некорректное значение! пожалуйста, введите целое число больше 1");
+            return enterQuantityUser(new Scanner(System.in));
+        }
+    }
+
+    private static int quantityUserChecker(int quantityUser, Scanner scanner) {
+        while (true) {
             if (quantityUser == 1) {
                 print("Нет смысла делить счет на одного :)");
+                quantityUser = enterQuantityUser(scanner);
             }
             if (quantityUser <= 0) {
                 print("Некорректное значение :(");
+                quantityUser = enterQuantityUser(scanner);
+
             }
             if (quantityUser > 1) {
                 break;
             }
         }
+        return quantityUser;
+
     }
-    private static void print (String message){
+
+    private static void print(String message) {
         System.out.println(message);
     }
 }
